f9bf8d44a98faa520c8a29e21c9bedf3d10b32e1
Abubker M <abubker.init@gmail.com>
2022-09-03 12:15:57 +0300

    [200~

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,17 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NumbersList from "./components/NumbersList";
 import PhonebookForm from "./components/PhonebookForm";
 import Search from "./components/Search";
-
+import axios from "axios";
 function App() {
   const [formState, setFormState] = useState({ name: "", phone: "" });
   const [keyword, setKeyword] = useState("");
-  const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-123456", id: 1 },
-    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
-    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
-    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
-  ]);
+  const [persons, setPersons] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setPersons(res.data))
+      .catch((err) => console.log(err));
+  }, []);
 
   const handelSearch = (e) => {
     setKeyword(() => e.target.value);
